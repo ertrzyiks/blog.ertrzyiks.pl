36b7d670c27e43091987b12f40c61354b1f2228c
ertrzyiks <mateusz.derks@gmail.com>
2017-06-05 20:46:28 +0200

    Hide ugly iframe borders

--- content/apps/my-ghost-tommy/index.js
+++ content/apps/my-ghost-tommy/index.js
@@ -6,8 +6,8 @@ function renderTommyIframe(slug) {
   return `
 <div class="post-iframe_placeholder--tommy">
   <div class="post-iframe_wrapper--tommy">
-    <button class="post-iframe_button--tommy is-hidden" style="display: none" data-role="load-exercise">Load the exercise</button>
-    <iframe class="post-iframe--tommy" data-src="${BASE_URL}${slug}" style="width: 100%; height: 600px"></iframe>
+    <button class="post-iframe_button--tommy is-hidden" style="display: none;" data-role="load-exercise">Load the exercise</button>
+    <iframe class="post-iframe--tommy" data-src="${BASE_URL}${slug}" style="width: 100%; height: 600px; border: none;"></iframe>
   </div>
 </div>
 `
